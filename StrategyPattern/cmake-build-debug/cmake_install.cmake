8ef8a9e8552bdb856c4077e30dfa6b727b70a99b
Nguyễn Bạch Trường Giang <22125020@student.hcmus.edu.vn>
2023-11-30 19:28:13 +0700

    little change

--- StrategyPattern/cmake-build-debug/cmake_install.cmake
+++ StrategyPattern/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/nguyenbachtruonggiang/Desktop/Seminar/StrategyPattern
+# Install script for directory: /Users/nguyenbachtruonggiang/Desktop/Seminar/g4_seminar_cs202/StrategyPattern
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/nguyenbachtruonggiang/Desktop/Seminar/StrategyPattern/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/nguyenbachtruonggiang/Desktop/Seminar/g4_seminar_cs202/StrategyPattern/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
